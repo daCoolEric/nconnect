5aeacafa81b1ffe7256ec7e59a84fa4eb4d6a04a
Dacooleric <dacooleric@gmail.com>
2023-10-22 00:36:43 +0000

    modified database.js

--- utils/database.js
+++ utils/database.js
@@ -3,18 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import "firebase/firestore";
 import "dotenv/config";
-const API_KEY = process.env.API_KEY;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: "nconnect-b9c97.firebaseapp.com",
-  projectId: "nconnect-b9c97",
-  storageBucket: "nconnect-b9c97.appspot.com",
-  messagingSenderId: "207231046975",
-  appId: "1:207231046975:web:71ef66213b04461613ef8a",
-  measurementId: "G-0ZEY0Q7FPF",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
