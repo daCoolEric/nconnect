a5cc46ac64c09b70333494c4918300ae15d3ea81
Rockson-devs <rockson.omane94@gmail.com>
2023-11-07 19:55:00 +0000

    worked on landing page and regional page

--- app/page.jsx
+++ app/page.jsx
@@ -5,10 +5,24 @@ const LandingPage = () => {
   return (
     // Mobile view
     <>
-      <div className="h-screen w-full bg-[url('/public/assets/images/background.png')]">
+      <div className=" relative h-screen w-full bg-[url('/assets/images/background-image.jpg')] ">
+        <div className=" absolute inset-0 bg-green-60"></div>
         <div className="h-1/4 outline">NConnect</div>
-        <div className="h-3/4 outline outline-black">
-          <Link href="/pages/signIn">Sign In</Link>
+        {/* <div className="h-3/4 outline outline-black"> */}
+        <div className="mb-1">
+          <p className=" text-6xl font-semibold text-slate-50 grid justify-items-center opacity-100">
+            Search, Locate <br /> & Contact any <br /> NIA office near <br />
+            you
+          </p>
+        </div>
+        <div className="grid justify-items-center">
+          <button className="bg-slate-50 w-5/6 mb-5 text-green-600 block p-3 text-2xl font-medium rounded-lg z-0">
+            <Link href="/pages/regional">Explore</Link>
+          </button>{" "}
+          <br />
+          <button className="bg-green-600 w-5/6 text-slate-50 block p-3  text-2xl font-medium rounded-lg z-0">
+            <Link href="/pages/signIn">Sign In</Link>
+          </button>
         </div>
       </div>
 
