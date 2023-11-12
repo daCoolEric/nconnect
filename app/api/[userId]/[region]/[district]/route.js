6804b17b0fc532e7ed27351752d64c369f7b8234
Dacooleric <dacooleric@gmail.com>
2023-11-12 09:06:52 +0000

    resolved api userID route

--- app/api/[userId]/[region]/[district]/route.js
+++ app/api/[userId]/[region]/[district]/route.js
@@ -1,30 +1,30 @@
-import {
-  collection,
-  addDoc,
-  getDocs,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from "firebase/firestore";
-import { db } from "@utils/database";
+// import {
+//   collection,
+//   addDoc,
+//   getDocs,
+//   deleteDoc,
+//   doc,
+//   updateDoc,
+// } from "firebase/firestore";
+// import { db } from "@utils/database";
 
-// GET REQUEST FOR SPECIFIC DISTRICT IN A SPECIFIC REGION
-const getInfos = async () => {
-  const querySnapshot = await getDocs(collection(db, "/ashanti"));
+// // GET REQUEST FOR SPECIFIC DISTRICT IN A SPECIFIC REGION
+// const getInfos = async () => {
+//   const querySnapshot = await getDocs(collection(db, "/ashanti"));
 
-  querySnapshot.forEach((doc) => {
-    let subin;
-    console.log(`${doc.id} => ${doc.data()}`);
-    // doc.id == "subin" ? (subin = doc.data()) : null;
-    // console.log(subin);
-    // const task = {
-    //   id: doc.id,
-    //   taskName: doc.data().taskName,
-    //   completed: doc.data().completed,
-    // };
-    // dispatch(setTodoList(task.taskName !== "" ? task : todoList));
-  });
+//   querySnapshot.forEach((doc) => {
+//     let subin;
+//     console.log(`${doc.id} => ${doc.data()}`);
+//     // doc.id == "subin" ? (subin = doc.data()) : null;
+//     // console.log(subin);
+//     // const task = {
+//     //   id: doc.id,
+//     //   taskName: doc.data().taskName,
+//     //   completed: doc.data().completed,
+//     // };
+//     // dispatch(setTodoList(task.taskName !== "" ? task : todoList));
+//   });
 
-  // return dispatch(setTodoList(returnData));
-};
-getInfos();
+//   // return dispatch(setTodoList(returnData));
+// };
+// getInfos();
