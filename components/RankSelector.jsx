599c8d1b690090feb725aa76d747aff4866c9957
Dacooleric <dacooleric@gmail.com>
2023-11-18 00:06:32 +0000

    made the selector background tranparent

--- components/RankSelector.jsx
+++ components/RankSelector.jsx
@@ -14,7 +14,7 @@ function RankSelector({ name }) {
         onChange={(e) => {
           setValue(e.target.value);
         }}
-        className="block w-full rounded-md border border-green-400 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 //focus:ring-2 //focus:ring-inset focus:outline-green-500 sm:text-sm sm:leading-6"
+        className="block w-full rounded-md bg-transparent border border-green-400 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 //focus:ring-2 //focus:ring-inset focus:outline-green-500 sm:text-sm sm:leading-6"
       >
         {name === "ranks"
           ? ranksDB.map((rank) => (
--- components/RegionsSelector.jsx
+++ components/RegionsSelector.jsx
@@ -20,7 +20,7 @@ function RegionsSelector({ name }) {
           dispatch(setDistricts(e.target.value));
           setValue(e.target.value);
         }}
-        className="block w-full rounded-md border border-green-400 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 //focus:ring-2 //focus:ring-inset focus:outline-green-500 sm:text-sm sm:leading-6"
+        className="block w-full rounded-md bg-transparent border border-green-400 pl-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 //focus:ring-2 //focus:ring-inset focus:outline-green-500 sm:text-sm sm:leading-6"
       >
         {name === "regions"
           ? regionDB.map((region) => (
