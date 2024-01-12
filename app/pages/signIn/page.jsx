bffe739c13e202ac1415c9a25ea9bb0695ffe43b
mac <dacooleric@gmail.com>
2024-01-12 09:38:09 +0000

    removed the infinite loop

--- app/pages/signIn/page.jsx
+++ app/pages/signIn/page.jsx
@@ -16,72 +16,66 @@ function SignIn() {
 
   return (
     <div className="h-3/4 w-screen  //outline //outline-black">
-      {true ? (
-        <>
-          <div className="flex min-h-full flex-1 flex-col justify-start px-6 py-3 lg:px-8 //outline //outline-black">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm //outline //outline-black">
-              <h2
-                className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight"
-                style={{ color: "#6dab3c" }}
+      <div className="flex min-h-full flex-1 flex-col justify-start px-6 py-3 lg:px-8 //outline //outline-black">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm //outline //outline-black">
+          <h2
+            className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight"
+            style={{ color: "#6dab3c" }}
+          >
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-green-600"
               >
-                Sign in to your account
-              </h2>
+                Email
+              </label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                onChange={(e) => dispatch(setEmail(e.target.value))}
+                autoComplete="email"
+                placeholder="Enter your email (ex. ayieric7@gmail.com)"
+              />
             </div>
 
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form className="space-y-6" action="#" method="POST">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-green-600"
-                  >
-                    Email
-                  </label>
-                  <Input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={(e) => dispatch(setEmail(e.target.value))}
-                    autoComplete="email"
-                    placeholder="Enter your email (ex. ayieric7@gmail.com)"
-                  />
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-green-600"
-                  >
-                    Password
-                  </label>
-                  <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={(e) => dispatch(setPassword(e.target.value))}
-                    autoComplete="password"
-                    placeholder="Enter your password"
-                  />
-                </div>
-                <div className="text-sm">
-                  <a
-                    href="/pages/forgotPassword/"
-                    className="font-semibold text-red-600 hover:text-red-400"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-
-                <div className="h-16">
-                  <Button id="signIn" type="button" name="Sign In" />
-                </div>
-              </form>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-green-600"
+              >
+                Password
+              </label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                onChange={(e) => dispatch(setPassword(e.target.value))}
+                autoComplete="password"
+                placeholder="Enter your password"
+              />
             </div>
-          </div>
-        </>
-      ) : (
-        <Regions />
-      )}
+            <div className="text-sm">
+              <a
+                href="/pages/forgotPassword/"
+                className="font-semibold text-red-600 hover:text-red-400"
+              >
+                Forgot password?
+              </a>
+            </div>
+
+            <div className="h-16">
+              <Button id="signIn" type="button" name="Sign In" />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
