a2ce97b9365a0ca20a72cb0b51bfba54791a107c
mac <dacooleric@gmail.com>
2024-01-27 21:03:37 +0000

    fix the cloudinary issue

--- app/api/[userId]/[region]/[district]/update_profile/route.js
+++ app/api/[userId]/[region]/[district]/update_profile/route.js
@@ -1,17 +1,23 @@
 import { NextResponse } from "next/server";
 import path from "path";
 import { writeFile } from "fs/promises";
-import { uploads } from "@backend/utils/cloudinary";
+import { uploadToCloudinary, uploads } from "@backend/utils/cloudinary";
 
 export const POST = async (req, res) => {
   const formData = await req.formData();
 
   const file = formData.get("image");
+  const fileBuffer = await file.arrayBuffer();
   if (!file) {
     return NextResponse.json({ error: "No files received." }, { status: 400 });
   }
 
-  const buffer = Buffer.from(await file.arrayBuffer());
+  let mime = file.type;
+  let encoding = "base64";
+  let buffer = Buffer.from(fileBuffer).toString("base64");
+  let fileUri = "data:" + mime + ";" + encoding + "," + buffer;
+
+  //   const buffer = Buffer.from(await file.arrayBuffer());
   const filename = Date.now() + file.name.replaceAll(" ", "_");
   const filePath = path.join(
     process.cwd(),
@@ -19,16 +25,50 @@ export const POST = async (req, res) => {
   );
   console.log(filename);
   console.log(buffer);
+
   try {
-    await writeFile(filePath, buffer);
-    console.log(filePath);
+    // const uploadToCloudinary = () => {
+    //     return new Promise((resolve, reject) => {
 
-    const uploader = (path) => uploads(path, "nconnect/profile_photos");
+    //         var result = cloudinary.uploader.upload(fileUri, {
+    //           invalidate: true
+    //         })
+    //           .then((result) => {
+    //             console.log(result);
+    //             resolve(result);
+    //           })
+    //           .catch((error) => {
+    //             console.log(error);
+    //             reject(error);
+    //           });
+    //     });
+    //   };
 
-    uploader(filePath);
+    //   const result = await uploadToCloudinary();
+
+    //   let imageUrl = result.secure_url;
+
+    //   return NextResponse.json(
+    //     { success: true, imageUrl: imageUrl },
+    //     { status: 200 }
+    const result = await uploadToCloudinary(fileUri, "nconnect/profile_photos");
+
+    let imageUrl = result.secure_url;
+
+    return NextResponse.json(
+      { success: true, imageUrl: imageUrl },
+      { status: 200 }
+    );
+
+    // await writeFile(filePath, buffer);
+    // console.log(filePath);
+
+    // const uploader = (path) => uploads(path, "nconnect/profile_photos");
+
+    // uploader(filePath);
 
     // newUserData.avatar = avatarResponse;
-    return NextResponse.json({ formData, Message: "Success", status: 201 });
+    // return NextResponse.json({ formData, Message: "Success", status: 201 });
   } catch (error) {
     console.log("Error occured ", error);
     return NextResponse.json({ Message: "Failed", status: 500 });
--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -5,19 +5,38 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
-const uploads = (filePath, folder) => {
-  cloudinary.uploader
-    .upload(filePath, { folder })
-    .then((result) => {
-      console.log({
-        public_id: result.public_id,
-        url: result.url,
+// const uploads = (filePath, folder) => {
+//   cloudinary.uploader
+//     .upload(filePath, { folder })
+//     .then((result) => {
+//       console.log({
+//         public_id: result.public_id,
+//         url: result.url,
+//       });
+//       unlink(filePath);
+//     })
+//     .catch((error) => console.warn(error));
+// };
+
+const uploadToCloudinary = (fileUri, folder) => {
+  return new Promise((resolve, reject) => {
+    var result = cloudinary.uploader
+      .upload(fileUri, {
+        invalidate: true,
+        folder,
+      })
+      .then((result) => {
+        console.log(result);
+        resolve(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
       });
-      unlink(filePath);
-    })
-    .catch((error) => console.warn(error));
+  });
 };
 
-export { uploads, cloudinary };
+export { uploadToCloudinary, cloudinary };
