b411738d8b7ba901f00775932895b60949eea617
mac <dacooleric@gmail.com>
2024-02-17 12:15:58 +0000

    corrected the callback and redirect error

--- app/pages/[userId]/explore/[region]/[district]/page.jsx
+++ app/pages/[userId]/explore/[region]/[district]/page.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import Office from "@components/Office";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 function District() {
   const session = useSession({
@@ -16,7 +17,7 @@ function District() {
   const officeAvailable = useSelector((state) => state.office.value);
   return (
     <div className="h-3/4 w-screen">
-      {officeAvailable ? (
+      {officeAvailable && (session.data?.user || uuidv4()) ? (
         <Office />
       ) : session.data?.user ? (
         <EmptyOffice />
